edd230e2af4230ec6ee3bb4a2a560185014a2e9f
Rokuto <gauven.boro@gmail.com>
2018-07-04 08:55:30 +0800

    NPM Usage.

--- app/index.js
+++
@@ -1,7 +0,0 @@
-// import { add } from 'calc';
-const calc = require('./calc');
-
-const numberToAdd = [3, 4, 10, 2];
-
-const result = calc.sum(numberToAdd);
-console.log(`The result is ${ result }`);
--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-require('./app/index');
+const _ = require('lodash');
+
+_.assign( { 'a':1 }, { 'b':2 }, { 'c':3 } );
+
+/*
+	Scoped or Private Packages
+	installation: npm i --save @myorg/mypackage
+	usage: require('@myorg/mypackage');
+*/
+

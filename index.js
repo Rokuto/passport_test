aa82384f1d3e7d34e0c5e0b8e2c43699b8c4afce
Rokuto <gauven.boro@gmail.com>
2018-07-04 13:56:19 +0800

    Async/Await in Node JS

--- index.js
+++ index.js
@@ -1,10 +1,58 @@
-const _ = require('lodash');
+const fs = require('fs');
 
-_.assign( { 'a':1 }, { 'b':2 }, { 'c':3 } );
+/* Synchronous way of reading a file
+let content;
+try {
+	content = fs.readFileSync('README.md', 'utf-8');
+} catch (err) {
+	console.log(err);
+}
 
-/*
-	Scoped or Private Packages
-	installation: npm i --save @myorg/mypackage
-	usage: require('@myorg/mypackage');
+console.log(content);
 */
+
+/* Error-first callback way of reading a file
+fs.readFile('README.md', 'utf-8', function (error, content) {
+	if (error) {
+		return console.log(error);
+	}
+
+	console.log(content);
+});
+*/
+
+/* Something went wrong
+console.log('Reading File . . .');
+fs.readFile('README.md', 'utf-8', (err, content) => {
+	if (err) return console.log(err);
+
+	console.log(content);
+});
+console.log('Done Reading File.');
+*/
+
+
+function stats (file) {
+	return new Promise ( (resolve, reject) => {
+		fs.readFile(file, (err, data) => {
+			if (err) reject(err);
+			resolve(data);
+		});
+	} ); 
+}
+
+async function asyncCall () {
+	console.log('Reading File . . .');
+	
+	console.log(`The file is: \n${ await stats('README.md') }`);
+	console.log(`The file is: \n${ await stats('index.js') }`);
+	// console.log(`The file is: \n${ await Promise.all( [stats('README.md'), stats('index.js')] ) }`);
+	
+	console.log('Done Reading File.');
+}
+
+asyncCall();
+
+// Filter Array and get all the nnumbers that are bigger than 2
+// console.log([2,4,1,5,3].filter( num => num > 2 ));
 
